38b2b8ad3870e5af4e13f7841922e9209347e559
Kian AliAkbari <kiana@kaytu.io>
2024-03-27 20:01:05 +0400

    base project

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import Container from './components/Container.vue'
 </script>
 
 <template>
-    <Container>
-        <RouterView />
-    </Container>
+    <div class="flex min-h-screen w-full justify-center bg-slate-50">
+        <Container>
+            <RouterView />
+        </Container>
+    </div>
 </template>
--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,7 +1,5 @@
 <template>
-    <div class="flex w-full justify-center">
-        <div class="relative w-full max-w-[1280px] p-4 text-sm text-gray-900">
-            <slot />
-        </div>
+    <div class="relative w-full max-w-[1280px] p-4 text-sm text-gray-900">
+        <slot />
     </div>
 </template>
