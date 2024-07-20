11b95c7a33360cd749b52ce8273aba4adfc564eb
Kian AliAkbari <kiana@kaytu.io>
2024-07-20 13:53:12 +0400

    add: jotai

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
+import { PrimeReactProvider } from 'primereact/api'
+import { Provider } from 'jotai'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const applyTheme = async () => {
+    const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)')
+    if (darkThemeMq.matches) {
+        await import('primereact/resources/themes/lara-dark-indigo/theme.css')
+    } else {
+        await import('primereact/resources/themes/lara-light-cyan/theme.css')
+    }
+}
+
+applyTheme().then(() => {
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+        <React.StrictMode>
+            <Provider>
+                <PrimeReactProvider>
+                    <App />
+                </PrimeReactProvider>
+            </Provider>
+        </React.StrictMode>
+    )
+})
